58128bfdc1b8735e3dd231dfbf4f4d5a125460d8
Steve Thomson <steve.thomson@shopify.com>
2025-10-31 12:22:56 +0000

    remove everything (#45)

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const {
-  getMinisMetroConfig,
-} = require('@shopify/shop-minis-runtime/metro-config')
-
-/**
- * @returns {import('metro-config').ConfigT}
- */
-module.exports = getMinisMetroConfig(__dirname)
--- packages/shop-minis-ui-extensions/src/extensions/marketing-card/types.ts
+++
@@ -1,5 +0,0 @@
-import {AVAILABLE_ICONS} from '@shopify/shop-minis-platform-sdk'
-
-type IconName = Exclude<(typeof AVAILABLE_ICONS)[number], undefined | number>
-
-export {IconName}
